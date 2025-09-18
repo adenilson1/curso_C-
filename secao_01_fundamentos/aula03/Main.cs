35812948f94febcfd8a2ba28d4e52ea5b14ecc6c
adenilson1 <adenilson99@hotmail.com>
2025-09-18 11:47:57 -0300

    variaveis e tipos basicos de dados

--- secao_01_fundamentos/aula03/Main.cs
+++ secao_01_fundamentos/aula03/Main.cs
@@ -5,7 +5,9 @@ class Program
     static void Main()
     {
         //Estrutura: tipo nome = valor
-        string texto01 = "Olá, Mundo!";
+        string texto01 = "Olá, Mundo!"; // aspas duplas
         Console.WriteLine(texto01);
+        char letra = 'b'; // aspas simples
+        Console.WriteLine(letra);
     }
 }
