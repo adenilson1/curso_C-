c672c822647edeec3d6b168ec96eceb7d46197c7
adenilson1 <adenilson99@hotmail.com>
2025-09-18 18:58:46 -0300

    Constantes

---
+++ secao_01_fundamentos/aula11/Main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //constantes
+
+        const int num1 = 20; //constante
+        num1 += 30; //erro
+        Console.WriteLine(num1);
+
+
+    }
+}
