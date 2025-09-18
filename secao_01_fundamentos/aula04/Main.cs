196326fc144f5eefa429b4d35a1a8aa6a1dc35d7
adenilson1 <adenilson99@hotmail.com>
2025-09-18 14:15:27 -0300

    tipos numericos inteiros

---
+++ secao_01_fundamentos/aula04/Main.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        //Tipos de dados numericos intereios
+
+        //inteiros
+        int num1 = 1000;
+        Console.WriteLine(num1);
+
+        //byte
+        byte num2 = 0; // 0 -255
+        Console.WriteLine(num2);
+    }
+}
