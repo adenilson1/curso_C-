7aafcf0ba74325e84ed7a8d75b24ba37a90aa2d6
adenilson1 <adenilson99@hotmail.com>
2025-09-18 20:51:36 -0300

    Classe Math

---
+++ secao_01_fundamentos/aula14/Main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Class Math
+        Console.WriteLine(Math.Pow(2, 2)); // potencia
+        Console.WriteLine(Math.Sqrt(4)); // raiz quadrada
+        Console.WriteLine(Math.Round(3.4)); // arrendondamento para o numero mais proximo
+        Console.WriteLine(Math.Ceiling(3.4)); // arrendondamento para o numero mais a cima
+        Console.WriteLine(Math.Floor(3.4)); // arrendondamento para o numero mais abaixo
+        Console.WriteLine(Math.Max(2,4)); // função max
+        Console.WriteLine(Math.Min(2,4)); // função min
+
+    }
+}
