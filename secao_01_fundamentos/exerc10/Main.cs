479c1309dbdb6f5d06a347d9d11494cfeb4cb57b
adenilson1 <adenilson99@hotmail.com>
2025-09-19 11:01:38 -0300

    Calculo de tempo de Download

--- secao_01_fundamentos/exerc10/Main.cs
+++ secao_01_fundamentos/exerc10/Main.cs
@@ -10,7 +10,7 @@ class Program
         Console.WriteLine("Qual é a velocidade da internet em Mbps? ");
         double velocidade_internet = Convert.ToDouble(Console.ReadLine());
 
-        double tempo_download = Math.Floor(tamanho_arquivo / velocidade_internet);
+        double tempo_download = Math.Floor(tamanho_arquivo * 8 / velocidade_internet);
         Console.WriteLine(
             string.Format(
                 "O arquivo de {0}MB e com velocidade de {1}Mbps, o tempo de Download é {2}s",
