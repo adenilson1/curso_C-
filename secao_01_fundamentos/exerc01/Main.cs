a03be73d14e0187cae14446182526166c3d61c42
adenilson1 <adenilson99@hotmail.com>
2025-09-19 10:12:49 -0300

    Entrada de um numero

---
+++ secao_01_fundamentos/exerc01/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Programa que mostra um numero
+        Console.WriteLine("Digite um numero inteiro");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("O numero digitado foi " + numero);
+    }
+}
