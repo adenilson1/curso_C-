2ffd04338d47b02e2da45eb74484671b41e0fd86
adenilson1 <adenilson99@hotmail.com>
2025-09-19 15:04:37 -0300

    Operadores relacionais

---
+++ secao_02_estrutura_de_controle/aula01/Main.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //operadores 
+        // bool value = false;
+
+        int numero1 = 5;
+        int numero2 = 5;
+
+        bool value1 = numero1 == numero2;
+        bool value2 = numero1 != numero2;
+        bool value3 = numero1 > numero2;
+        bool value4 = numero1 < numero2;
+        bool value5 = numero1 >= numero2;
+        bool value6 = numero1 <= numero2;
+
+
+        Console.WriteLine(value1);
+        Console.WriteLine(value2);
+        Console.WriteLine(value3);
+        Console.WriteLine(value4);
+        Console.WriteLine(value5);
+        Console.WriteLine(value6);
+    }
+}
