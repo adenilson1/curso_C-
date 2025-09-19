5053cede29ab106d0e1bb83ff068e15805e9d40a
adenilson1 <adenilson99@hotmail.com>
2025-09-19 10:18:45 -0300

    Calculo de velocidade de Download

---
+++ secao_01_fundamentos/exerc10/Main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Calculo de Download
+        Console.WriteLine("Qual o tamanho do arquivo em MB?");
+        double tamanho_arquivo = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Qual é a velocidade da internet em Mbps? ");
+        double velocidade_internet = Convert.ToDouble(Console.ReadLine());
+
+        double tempo_download = Math.Floor(tamanho_arquivo / velocidade_internet);
+        Console.WriteLine(
+            string.Format(
+                "O arquivo de {0}MB e com velocidade de {1}Mbps, o tempo de Download é {2}s",
+                 tamanho_arquivo, velocidade_internet, tempo_download
+            )
+        );
+    }
+}
