32a057f5127c88bf81a8ac43e08e522970733840
adenilson1 <adenilson99@hotmail.com>
2025-09-20 11:40:25 -0300

    Loop While

---
+++ secao_03_estrutura_de_controle_repeicao/aula02/Main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Principal
+{
+    static void Main()
+    {
+        int n1 = 0;
+        while (n1 < 10)
+        {
+            Console.WriteLine("Olá, Mundo! " + n1);
+            n1++;
+        }
+    }
+}
