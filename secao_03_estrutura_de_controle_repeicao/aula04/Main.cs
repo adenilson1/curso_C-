465c2429044b544a427104f063c414d938052ad5
adenilson1 <adenilson99@hotmail.com>
2025-09-20 13:37:32 -0300

    Caracteristicas do loop

---
+++ secao_03_estrutura_de_controle_repeicao/aula04/Main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Principal
+{
+    static void Main()
+    {
+        int n1 = 0; //inicialização
+
+        while (n1 < 10) // teste logico
+        {
+            n1++; // incremento ou decremento
+        }
+    }
+}
