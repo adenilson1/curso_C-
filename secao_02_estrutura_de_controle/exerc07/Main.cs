af0edfb9c9d11a3450b89618a9778a3c14e8e270
adenilson1 <adenilson99@hotmail.com>
2025-09-19 21:10:51 -0300

    Operações

---
+++ secao_02_estrutura_de_controle/exerc07/Main.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite dois numeros");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        inicio:
+        Console.WriteLine("Escolha a operação que deseja: +, -, *, /");
+        Console.WriteLine("[1] - Soma");
+        Console.WriteLine("[2] - Subtração");
+        Console.WriteLine("[3] - Multiplicação");
+        Console.WriteLine("[4] - Divisão");
+        int opcao = Convert.ToInt32(Console.ReadLine());
+  
+
+        switch (opcao)
+        {
+            case 1: Console.WriteLine(numero1 + numero2); break;
+            case 2: Console.WriteLine(numero1 - numero2); break;
+            case 3: Console.WriteLine(numero1 * numero2); break;
+            case 4: Console.WriteLine(numero1 / numero2); break;
+            default: Console.WriteLine("Opção inválida"); goto inicio; break;
+        }
+
+    }
+}
