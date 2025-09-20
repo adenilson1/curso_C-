636a96cc427ceb9c5933eca079c6103b1ef0af3c
adenilson1 <adenilson99@hotmail.com>
2025-09-20 13:44:13 -0300

    Loop do-while

---
+++ secao_03_estrutura_de_controle_repeicao/aula05/Main.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+class Principal
+{
+    static void Main()
+    {
+        int n1 = 0;
+        do
+        {
+            n1++;
+            Console.WriteLine(n1);
+        } while (n1 < 10);
+    }
+}
