044cd1ef074c8cb30fce77a78b04a6bf8e2c69a7
adenilson1 <adenilson99@hotmail.com>
2025-09-20 14:03:18 -0300

    Loop For

---
+++ secao_03_estrutura_de_controle_repeicao/aula06/Main.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Principal
+{
+    static void Main()
+    {
+        for (int num = 0; num < 10; num++)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.WriteLine("------------------------");
+
+        for (int num = 10; num > 0; num--)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.WriteLine("------------------------");
+
+        Console.WriteLine("Tabuada");
+        for (int num = 1; num < 10; num++)
+        {
+            for (int num1 = 0; num1 < 10; num1++)
+            {
+                Console.WriteLine(num + " * " + num1 + " = " + num*num1);
+            }
+        }
+    }
+}
