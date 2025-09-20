d3a5653db39c5be70eaf51ab774bfb38b0ce5eb3
adenilson1 <adenilson99@hotmail.com>
2025-09-20 11:21:00 -0300

    Operadores de incremento e decremento

---
+++ secao_03_estrutura_de_controle_repeicao/aula01/Main.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Principal
+{
+    static void Main()
+    {
+        //Incremento
+        // int n1 = 20;
+        // n1--;
+        // Console.WriteLine(n1);
+
+        //posfixo
+        // int n1 = 20;
+        // Console.WriteLine(n1);
+        // Console.WriteLine(n1++);
+        // Console.WriteLine(n1);
+
+        //prefixo
+        int n1 = 20;
+        Console.WriteLine(n1);
+        Console.WriteLine(++n1);
+        Console.WriteLine(n1);
+
+    }
+}
