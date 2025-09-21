4a53d24d68ed9b01119f9cb6fb879efb7575f540
adenilson1 <adenilson99@hotmail.com>
2025-09-21 09:41:46 -0300

    Metodo simples

---
+++ secao_04_poo_i/aula01/Main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Principal // classe
+{
+    static void Main() // metodo pricipal.so aqui aconte as coisa
+    {
+        Metodo1();// chamando metodo1
+    }
+    //retorno, nome()
+    static void Metodo1(/*parametros*/)
+    {
+        //Voide - > siginfica sem retorno
+        Console.WriteLine("Olá, Mundo!");
+    }
+}
