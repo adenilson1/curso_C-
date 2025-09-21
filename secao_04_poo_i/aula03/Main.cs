ca0bb50ae075d78ed2ce615bfb5478f872403eb3
adenilson1 <adenilson99@hotmail.com>
2025-09-21 10:55:59 -0300

    Escopo e Retorno

---
+++ secao_04_poo_i/aula03/Main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Principal
+{
+    static void Main()
+    {
+        int res = Soma(10, 5);
+        Console.WriteLine(res);
+    }
+
+    static int Soma(int n1, int n2)// metdo que retorna valor
+    {
+        return n1 + n2;
+        
+    }
+}
