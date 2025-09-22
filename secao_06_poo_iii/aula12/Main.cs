2a80bac0d7ece7123ff002be0e24da4fe4b579e9
adenilson1 <adenilson99@hotmail.com>
2025-09-22 16:45:04 -0300

    Namespaces

---
+++ secao_06_poo_iii/aula12/Main.cs
@@ -0,0 +1,32 @@
+using System;
+
+// namespace Aula //diretorio
+// {
+//     class Program // nome do arquivo
+//     {
+//         static void Main(string[] args) // metodo principal
+//         {
+//             Console.WriteLine("Olá, Mundo!");
+//         }
+//     }
+// }
+
+namespace Name
+{
+    class Principal
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
+
+// namespace Outro
+// {
+    
+//     class principal
+//     {
+//         public int n1;
+//     }
+// }
