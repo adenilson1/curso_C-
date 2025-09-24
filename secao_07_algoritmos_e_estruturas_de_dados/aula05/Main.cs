4f254d98c4e1fc87e82bac2bf3404792b7469766
adenilson1 <adenilson99@hotmail.com>
2025-09-24 12:16:36 -0300

    Lista

---
+++ secao_07_algoritmos_e_estruturas_de_dados/aula05/Main.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+class Principal
+{
+    static void Main()
+    {
+
+        List<int> Lista1 = new List<int>() { 10, 20, 30 };
+        // Lista1.Add(10);
+        // Lista1.Add(20);
+        // Lista1.Remove(20);
+        // Lista1.Insert(2, 40); // inserindo valor em uma determinada posição. valor 40 na posição 2
+        // foreach (var item in Lista1)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        // bool ex = Lista1.Contains(20);
+
+        // if (Lista1.Contains(20))
+        // {
+
+        // }
+
+        // int n = Lista1.IndexOf(20);
+
+
+    }
+}
